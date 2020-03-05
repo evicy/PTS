e72d5216c5f85eea965c7fb321e94b8f64470ddd
evi <herencsareva@gmail.com>
2020-03-05 21:54:00 +0100

    Testing cases added for the first half of methods

--- relations.py
+++ relations.py
@@ -28,3 +28,53 @@ class Relation:
 
     def substraction(self, rel):
         return Relation(self.set, self.relation.difference(rel.relation))
+
+############ TESTING ##############
+
+print("---Initializing, contains, add, remove---")
+
+r = Relation({1, 2, 3})
+print(r.__contains__(1))
+print(r.relation)
+
+r = r.add((1, 2))
+r = r.add((2, 3))
+r = r.add((1, 3))
+print(r.relation)
+
+r = r.add((5, 3))
+print('Contains (5,3)? ', r.__contains__((5,3)))
+print(r.relation)
+
+r = r.remove((5, 3))
+print('r after removing (5,3) ', r.relation)
+
+r = r.remove((1,3))
+print('r after removing (1,3) ', r.relation)
+
+p = Relation({2, 3, 4})
+p = p.add((2, 3))
+p = p.add((3, 4))
+print('p: ', p.relation,'\n')
+
+
+print("-------UNION---------")
+print('r=', r.relation)
+print('p=', p.relation)
+q = r.union(p)
+print('q = r U p = ', q.relation, ' M = ', q.set, '\n')
+
+print("----INTERSECTION----")
+print('r=', r.relation)
+print('p=', p.relation)
+q = r.intersection(p)
+print('q = r intersect p=', q.relation, ' M = ', q.set)
+e = Relation({1,2,3,4,5,6})
+q = r.intersection(e)
+print('q = r intersect with empty ', q.relation, ' M = ', q.set, '\n')
+
+print("-----SUBSTRACTION-----")
+print('r=', r.relation)
+print('p=', p.relation)
+q = r.substraction(p)
+print('q = r U p = ', q.relation, ' M = ', q.set, '\n')
